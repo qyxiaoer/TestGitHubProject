d65d8ce3f5af40e276e608b20ec78a05e2bb1a7a
qyxiaoer <692108438@qq.com>
2018-11-29 09:32:13 +0800

    Initial commit

---
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -0,0 +1,14 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:zl
+ * createTime:2018-11-29 09:06
+ */
+
+public class DeptController {
+    public static void main(String[] args) {
+        System.out.println("测试github提交11111111111");
+    }
+}
